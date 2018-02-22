cd6f1336c324807496f0fc78b974e773fc334ec1
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-02-22 13:47:00 -0600

    update footer with copyright and change script source on map.html file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 "use strict";
+console.log("hello MAIN js");
 
 let testGrab = require("./fire");
 let testingGrab = require("./map");
@@ -18,3 +19,27 @@ testGrab.area1(1)
 });
 
 //FUNCTIONS TO POST DATA TO DOM BY ID ///////////////////////
+
+
+//Copyright//
+var currentDate = new Date();
+var monthIndex = new Array();
+    monthIndex[0] = "January";
+    monthIndex[1] = "February";
+    monthIndex[2] = "March";
+    monthIndex[3] = "April";
+    monthIndex[4] = "May";
+    monthIndex[5] = "June";
+    monthIndex[6] = "July";
+    monthIndex[7] = "August";
+    monthIndex[8] = "September";
+    monthIndex[9] = "October";
+    monthIndex[10] = "November";
+    monthIndex[11] = "December";
+var month = monthIndex[currentDate.getMonth()];
+var year = currentDate.getFullYear();
+var copyright = document.getElementById("copyright");
+function showCopyright(){
+    copyright.innerHTML = `&copy; ${month} ${year}. Unitainment Corp. <br />All rights reserved.`;
+};
+showCopyright();
