acde53868deea93f0956fcd2882342ee38db487f
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-02-23 13:25:40 -0600

    modified js modules and map html to show attractions on click

--- js/print.js
+++ js/print.js
@@ -1,28 +1,46 @@
 "use strict";
+console.log("PRINTjs HAS ARRIVED!");
+//REQUIRES
+// let {grabArea, dataArea} = require('./fire');
+let fetchFire = require('./fire');
+let displayArea = require('./map');
 
-let {grabArea} = require('./fire');
+//EMPTY ARRAYS
+let areaArray = [];
+let attractionsArray = [];
+let timesArray = [];
+let attractionsInfo = [];
 
-
+//VARIABLES
 let printFire = document.getElementById("areaList");
+var attractionData;
 
 
-function fetchFirePrint(id){     //wrapped in function so .then doesn't fire on its own
-grabArea(id)
-    .then(
-        (data) => {
-           printFire.innerHTML = `<h2>AREA${id}</h2>`;
-        console.log("attraction data", data);
-        Object.keys(data).forEach((item) => {   //Mike showed me this and i dont understand
-            var index = (data[item]);
-            console.log(index);
-            if(index.area_id === id){      // No longer need an if statement
-                printFire.innerHTML += `<div class=areaAttractions${id} id=${index.id}><h3>${index.name}</div>;`; 
-            }else if(index.area_id === id){
-                printFire.innerHTML += `<div class="newArea" id=${index.id}><h3>${index.name}</h3></div>`;
-            }
-        });
-        }
-    );
-}
+//FUNCTIONS
+function printAttractions(){
+    // console.log("SHOW UP printAttractions function");
+    printFire.innerHTML = `<h2>AREA${displayArea()}</h2>`;
+} 
 
-module.exports= {fetchFirePrint};
+printAttractions();
+
+// function fetchFirePrint(id){     //wrapped in function so .then doesn't fire on its own
+// grabArea(id)
+//     .then(
+//         (data) => {
+//            printFire.innerHTML = `<h2>AREA${id}</h2>`;
+//         console.log("attraction data", data);
+//         Object.keys(data).forEach((item) => {   //Mike showed me this and i dont understand
+//             var index = (data[item]);
+//             console.log(index);
+//             if(index.area_id === id){      // No longer need an if statement
+//                 printFire.innerHTML += `<div class=areaAttractions${id} id=${index.id}><h3>${index.name}</div>;`; 
+//             }else if(index.area_id === id){
+//                 printFire.innerHTML += `<div class="newArea" id=${index.id}><h3>${index.name}</h3></div>`;
+//             }
+//         });
+//         }
+//     );
+// }
+
+module.exports= {printAttractions};
